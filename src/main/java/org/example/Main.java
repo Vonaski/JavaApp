689063f04bc931d4a48d314b3981e50f7aeb00a6
Vonaski <reflex.11.04.1999@gmail.com>
2025-01-22 21:34:27 +0500

    super feature fix bugs increase reliability

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iksanov");
         System.out.println("Ilyas");
+        System.out.println("This is super feature!");
     }
 }
